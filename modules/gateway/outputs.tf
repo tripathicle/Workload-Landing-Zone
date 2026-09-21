b2068276fb5a85e70862cb906411899f1dbbd3df
tripathicle <tripathicle@gmail.com>
2026-09-21 21:50:49 +0530

    Application gateway modified

--- modules/gateway/outputs.tf
+++ modules/gateway/outputs.tf
@@ -1,5 +1,7 @@
 output "application_gateways" {
+
   description = "Map of Application Gateways"
+
   value = {
     for key, gateway in azurerm_application_gateway.this : key => {
       id   = gateway.id
